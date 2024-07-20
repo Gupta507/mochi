b354e1809ec6e4775c397dba6cfa78d79f9fec93
Dave <davenlu1989@gmail.com>
2024-07-20 14:26:15 +0900

    chore: improve language

--- app/_components/popular.tsx
+++ app/_components/popular.tsx
@@ -7,35 +7,37 @@ import { getPopularAnime } from "@/providers/anime";
 import { AnimeList, AnimeListSkeleton } from "@/components/anime/anime-list";
 
 export default function Popular() {
-  const [popularAnime, setPopularAnime] = useState<AnilistResult[]>([]);
-  const [loading, setLoading] = useState(true);
+    const [popularAnime, setPopularAnime] = useState<AnilistResult[]>([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchPopularAnime() {
-      try {
-        const response = await getPopularAnime();
-        setPopularAnime(response.results);
-      } catch (error) {
-        console.error("Failed to fetch popular anime:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
+    useEffect(() => {
+        async function fetchPopularAnime() {
+            try {
+                const response = await getPopularAnime();
+                setPopularAnime(response.results);
+            } catch (error) {
+                console.error("Failed to fetch popular anime:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
 
-    fetchPopularAnime();
-  }, []);
+        fetchPopularAnime();
+    }, []);
 
-  return (
-    <div className="px-4 lg:px-8 py-6">
-      <div>
-        <h1 className="text-2xl font-medium">Popular Animes of All Time</h1>
-        {/* <p className="text-sm text-muted-foreground">{description}</p> */}
-      </div>
-      {loading ? (
-        <AnimeListSkeleton type="default" />
-      ) : (
-        <AnimeList type="default" list={popularAnime} />
-      )}
-    </div>
-  );
+    return (
+        <div className="px-4 lg:px-8 py-6">
+            <div>
+                <h1 className="text-2xl font-medium">
+                    Popular Anime (All Time)
+                </h1>
+                {/* <p className="text-sm text-muted-foreground">{description}</p> */}
+            </div>
+            {loading ? (
+                <AnimeListSkeleton type="default" />
+            ) : (
+                <AnimeList type="default" list={popularAnime} />
+            )}
+        </div>
+    );
 }
